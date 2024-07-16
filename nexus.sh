2ba8ae9f3182ade886843ee8b5fdd4e471548939
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-07-16 13:32:47 +0530

    nexus script

--- nexus.sh
+++ nexus.sh
@@ -62,3 +62,49 @@ NEXUSFILE="/opt/$NEXUSFILE"
 wget $URL -O $NEXUSFILE &>/dev/null
 if [ $? -eq 0 ]; then
     success "NEXUS Downloaded Successfully"
+else
+    error "NEXUS Downloading Failure"
+fi
+
+## Adding Nexus User
+id nexus &>/dev/null
+if [ $? -ne 0 ]; then
+    useradd nexus
+    if [ $? -eq 0 ]; then
+        success "Added NEXUS User Successfully"
+    else
+        error "Adding NEXUS User Failure"
+    fi
+fi
+
+## Extracting Nexus
+if [ ! -d "/home/nexus/$NEXUSDIR" ]; then
+    su nexus <<EOF
+cd /home/nexus
+tar xf $NEXUSFILE
+EOF
+    if [ $? -eq 0 ]; then
+        success "Extracted NEXUS Successfully"
+    else
+        error "Extracting NEXUS Failed"
+    fi
+fi
+
+## Setting Nexus startup
+unlink /etc/init.d/nexus &>/dev/null
+ln -s /home/nexus/$NEXUSDIR/bin/nexus /etc/init.d/nexus
+echo "run_as_user=nexus" >/home/nexus/$NEXUSDIR/bin/nexus.rc
+CONFIG_FILE=$(find /home/nexus/ -name nexus-default.properties)
+sed -i -e '/nexus.scripts.allowCreation/ d' $CONFIG_FILE
+sed -i -e '$ a nexus.scripts.allowCreation=true' $CONFIG_FILE
+pip3 install nexus3-cli &>/tmp/nexus-install.log
+
+success "Updating System Configuration"
+systemctl enable nexus &>/dev/null
+systemctl start nexus
+if [ $? -eq 0 ]; then
+    success "Starting Nexus Service"
+else
+    error "Starting Nexus Failed"
+fi
+
