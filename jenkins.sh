0ff437c3500563b4fdc7dd720039f88807d8b180
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-07-23 12:40:18 +0530

    nexus url updated

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-17-openjdk -y 
 yum install jenkins -y
